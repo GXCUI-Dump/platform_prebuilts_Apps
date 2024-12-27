9454d2f3519dbf87689f0a81f34fde66575d6c0f
GXC2356 <gxcdev2356@gmail.com>
2024-12-27 02:13:38 +0200

    Add some opensource prebuilt applications

---
+++ Calculator/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Calculator
+LOCAL_SRC_FILES := Calculator.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_OPTIONAL_USES_LIBRARIES := \
+    androidx.window.extensions \
+    androidx.window.sidecar
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)
